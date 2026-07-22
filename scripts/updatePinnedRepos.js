29d8757f6466f1170798d01c60d9728763aafe40
Md. Tanvir Jahan Shakib <mdtanvirjahanshakib@gmail.com>
2026-07-23 04:33:22 +0600

    Improve HTML table structure in updatePinnedRepos.js
    Updated table styling and padding for better layout.

--- scripts/updatePinnedRepos.js
+++ scripts/updatePinnedRepos.js
@@ -1,25 +1,29 @@
 function generateTable(repos) {
   let html = `
-<table width="100%" style="table-layout: fixed;">
+<table width="100%" style="table-layout:fixed;">
 <tbody>
 `;
 
   for (let i = 0; i < repos.length; i += 2) {
+
     html += `<tr>`;
 
     for (let j = i; j < i + 2; j++) {
 
       if (repos[j]) {
+
         const repo = repos[j];
 
         html += `
-<td width="50%" valign="top" style="width:50%; padding:20px;">
+<td width="50%" valign="top" style="width:50%; padding:10px;">
+
+<table width="100%" style="border:1px solid #30363d; border-radius:10px;">
+<tr>
+<td style="padding:20px;">
 
 <h3>
 📦 <a href="${repo.url}">
-${repo.name.length > 25 
-? repo.name.substring(0, 25) + "..." 
-: repo.name}
+${repo.name}
 </a>
 </h3>
 
@@ -27,14 +31,23 @@ ${repo.name.length > 25
 ${repo.description || "_No description available._"}
 </p>
 
+<p>
+🔗 <a href="${repo.url}">
+View Repository
+</a>
+</p>
+
+</td>
+</tr>
+</table>
+
 </td>
 `;
 
       } else {
 
         html += `
-<td width="50%" valign="top" style="width:50%; padding:20px;">
-</td>
+<td width="50%"></td>
 `;
 
       }
