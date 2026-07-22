3231443a4b235b60b0a262fcb9328ece5f85d4a2
Md. Tanvir Jahan Shakib <mdtanvirjahanshakib@gmail.com>
2026-07-23 04:59:36 +0600

    Refactor to fetch pinned repositories from GitHub

--- scripts/updatePinnedRepos.js
+++ scripts/updatePinnedRepos.js
@@ -1,38 +1,75 @@
 const username = "tanvirjahanshakib";
 
-async function getRepos() {
+async function getPinnedRepos() {
+
+  const query = `
+  query {
+    user(login: "${username}") {
+      pinnedItems(first: 6, types: REPOSITORY) {
+        nodes {
+          ... on Repository {
+            name
+            description
+            url
+            homepageUrl
+          }
+        }
+      }
+    }
+  }
+  `;
+
+
   const response = await fetch(
-    `https://api.github.com/users/${username}/repos?sort=updated`
+    "https://api.github.com/graphql",
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ query }),
+    }
   );
 
-  const data = await response.json();
 
-  const repos = data.map(repo => ({
+  const result = await response.json();
+
+
+  const repos = result.data.user.pinnedItems.nodes.map(repo => ({
     name: repo.name,
     description: repo.description,
-    url: repo.html_url,
-    homepage: repo.homepage
+    url: repo.url,
+    homepage: repo.homepageUrl
   }));
+
 
   return generateTable(repos);
 }
 
 
+
 function generateTable(repos) {
+
   let html = `
 <table width="100%" style="table-layout:fixed;">
 <tbody>
 `;
 
+
   for (let i = 0; i < repos.length; i += 2) {
 
     html += `<tr>`;
 
+
     for (let j = i; j < i + 2; j++) {
+
 
       if (repos[j]) {
 
+
         const repo = repos[j];
+
 
         html += `
 <td width="50%" valign="top" style="width:50%; padding:10px;">
@@ -41,21 +78,25 @@ function generateTable(repos) {
 <tr>
 <td style="padding:20px;">
 
+
 <h3>
 📦 <a href="${repo.url}">
 ${repo.name}
 </a>
 </h3>
 
+
 <p>
 ${repo.description || "_No description available._"}
 </p>
+
 
 <p>
 🔗 <a href="${repo.url}">
 View Repository
 </a>
 </p>
+
 
 ${repo.homepage ? `
 <p>
@@ -64,6 +105,7 @@ Live Demo
 </a>
 </p>
 ` : ""}
+
 
 </td>
 </tr>
@@ -74,26 +116,35 @@ Live Demo
 
       } else {
 
+
         html += `
 <td width="50%"></td>
 `;
 
       }
+
     }
 
+
     html += `</tr>`;
+
   }
+
+
 
   html += `
 </tbody>
 </table>
 `;
 
+
   return html;
+
 }
 
 
+
 // Generate README content
-getRepos().then(repoCards => {
+getPinnedRepos().then(repoCards => {
   console.log(repoCards);
 });
