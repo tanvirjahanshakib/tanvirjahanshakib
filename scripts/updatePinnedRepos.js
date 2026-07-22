2b640597aa0bbe811e14f5e2a34befe81198a579
Md. Tanvir Jahan Shakib <mdtanvirjahanshakib@gmail.com>
2026-07-23 04:28:12 +0600

    Refactor HTML table generation in updatePinnedRepos.js

--- scripts/updatePinnedRepos.js
+++ scripts/updatePinnedRepos.js
@@ -1,31 +1,52 @@
 function generateTable(repos) {
-  let html = `<table width="100%">`;
+  let html = `
+<table width="100%" style="table-layout: fixed;">
+<tbody>
+`;
 
   for (let i = 0; i < repos.length; i += 2) {
     html += `<tr>`;
 
     for (let j = i; j < i + 2; j++) {
+
       if (repos[j]) {
         const repo = repos[j];
 
         html += `
-<td width="49%" valign="top">
+<td width="50%" valign="top" style="width:50%; padding:20px;">
 
-### 📦 <a href="${repo.url}">${repo.name}</a>
+<h3>
+📦 <a href="${repo.url}">
+${repo.name.length > 25 
+? repo.name.substring(0, 25) + "..." 
+: repo.name}
+</a>
+</h3>
 
+<p>
 ${repo.description || "_No description available._"}
+</p>
 
 </td>
 `;
+
       } else {
-        html += `<td width="49%"></td>`;
+
+        html += `
+<td width="50%" valign="top" style="width:50%; padding:20px;">
+</td>
+`;
+
       }
     }
 
     html += `</tr>`;
   }
 
-  html += `</table>`;
+  html += `
+</tbody>
+</table>
+`;
 
   return html;
 }
