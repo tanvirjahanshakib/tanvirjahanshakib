2ff1e56338be88ab5f35c5ed055af82e60425b26
Md. Tanvir Jahan Shakib <mdtanvirjahanshakib@gmail.com>
2026-07-23 06:36:27 +0600

    Add detailed repository information in updatePinnedRepos
    Enhanced the getPinnedRepos function to include additional repository details such as stars, forks, and primary language. Updated the HTML generation in generateTable to reflect these changes.

--- scripts/updatePinnedRepos.js
+++ scripts/updatePinnedRepos.js
@@ -6,21 +6,30 @@ const username = "tanvirjahanshakib";
 async function getPinnedRepos() {
 
   const query = `
-  query {
-    user(login: "${username}") {
-      pinnedItems(first: 6, types: REPOSITORY) {
-        nodes {
-          ... on Repository {
+query {
+  user(login: "${username}") {
+    pinnedItems(first: 6, types: REPOSITORY) {
+      nodes {
+        ... on Repository {
+          name
+          description
+          url
+          homepageUrl
+          stargazerCount
+          forkCount
+
+          primaryLanguage {
             name
-            description
-            url
-            homepageUrl
+            color
           }
+
+          openGraphImageUrl
         }
       }
     }
   }
-  `;
+}
+`;
 
 
   const response = await fetch(
@@ -39,174 +48,120 @@ async function getPinnedRepos() {
   const result = await response.json();
 
 
-  const repos = result.data.user.pinnedItems.nodes.map(repo => ({
-    name: repo.name,
-    description: repo.description,
-    url: repo.url,
-    homepage: repo.homepageUrl
-  }));
+ const repos = result.data.user.pinnedItems.nodes.map(repo => ({
+  name: repo.name,
+  description: repo.description || "No description available.",
+  url: repo.url,
+  homepage: repo.homepageUrl,
+  stars: repo.stargazerCount,
+  forks: repo.forkCount,
+  language: repo.primaryLanguage?.name || "Unknown",
+  color: repo.primaryLanguage?.color || "#586069",
+  image: repo.openGraphImageUrl
+}));
 
 
   return generateTable(repos);
 }
 
-
-
 function generateTable(repos) {
 
   let html = `
-<table width="100%" style="table-layout:fixed;">
+<table width="100%" cellspacing="10" cellpadding="0">
 <tbody>
 `;
-
 
   for (let i = 0; i < repos.length; i += 2) {
 
     html += `<tr>`;
 
-
     for (let j = i; j < i + 2; j++) {
 
+      if (!repos[j]) {
+        html += `<td width="50%"></td>`;
+        continue;
+      }
 
-      if (repos[j]) {
+      const repo = repos[j];
 
-        const repo = repos[j];
+      html += `
+<td width="50%" valign="top">
 
-
-        html += `
-<td width="50%" valign="top" style="
-width:50%;
-padding:10px;
-">
-
-
-<table width="100%" style="
-border:1px solid #30363d;
-border-radius:12px;
-height:330px;
-">
-
+<table width="100%" cellpadding="16" cellspacing="0"
+style="border:1px solid #30363d; border-radius:12px;">
 
 <tr>
 
-<td style="
-padding:20px;
-vertical-align:top;
-">
+<td>
 
+<p align="center">
 
-<div style="
-height:270px;
-display:flex;
-flex-direction:column;
-">
+<a href="${repo.url}">
 
+<img src="${repo.image}" width="100%">
 
-<div>
+</a>
 
+</p>
 
-<h3 style="
-margin:0;
-height:35px;
-overflow:hidden;
-">
+<h3>
 
-📦 
-<a href="${repo.url}" target="_blank">
+📦
+<a href="${repo.url}">
 ${repo.name}
 </a>
 
 </h3>
 
-
-
-<div style="
-height:100px;
-overflow:hidden;
-margin-top:10px;
-">
-
 <p>
-${repo.description || "_No description available._"}
+
+${repo.description}
+
 </p>
 
-
-</div>
-
-
-</div>
-
-
-
-
-
-<div style="
-margin-top:auto;
-">
-
-
-
 <p>
-<a href="${repo.url}" target="_blank">
-<img src="https://img.shields.io/badge/GitHub-Repository-181717?style=for-the-badge&logo=github&logoColor=white">
-</a>
+
+<img src="https://img.shields.io/badge/Language-${encodeURIComponent(repo.language)}-blue?style=flat-square">
+
+<img src="https://img.shields.io/badge/⭐-${repo.stars}-yellow?style=flat-square">
+
+<img src="https://img.shields.io/badge/🍴-${repo.forks}-blue?style=flat-square">
+
 </p>
 
-
-
+<p>
 
 ${repo.homepage ? `
-<p>
-<a href="${repo.homepage}" target="_blank">
-<img src="https://img.shields.io/badge/Live-Demo-38BDF8?style=for-the-badge&logo=googlechrome&logoColor=white">
+<a href="${repo.homepage}">
+<img src="https://img.shields.io/badge/🌐-Live_Demo-2ea44f?style=for-the-badge">
 </a>
-</p>
 ` : ""}
 
+<a href="${repo.url}">
+<img src="https://img.shields.io/badge/🐙-Repository-181717?style=for-the-badge&logo=github&logoColor=white">
+</a>
 
-
-</div>
-
-
-
-</div>
-
+</p>
 
 </td>
 
 </tr>
 
-
 </table>
-
 
 </td>
 `;
-
-      } else {
-
-        html += `
-<td width="50%"></td>
-`;
-
-      }
-
     }
 
-
     html += `</tr>`;
-
   }
-
 
   html += `
 </tbody>
 </table>
 `;
 
-
   return html;
-
 }
 
 
